faa4a4b3b7093bd691646f173c85ed331d162118
AlohaCode <bretthooper.maui@gmail.com>
2017-03-08 15:25:57 +0000

    0603 partial Flux impl

--- demo-1/src/js/components/movies/Movies.js
+++ demo-1/src/js/components/movies/Movies.js
@@ -1,10 +1,26 @@
 import React from "react";
 
+import Movie from "../movie/Movie";
+import MoviesStore from "../../stores/MoviesStore";
+
 export default class Movies extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: MoviesStore.getMovies()
+    };
+  }
+  
   render() {
+    const { movies } = this.state;
+    const movieList = movies.map((movie) => {
+      return <Movie key={movie.episode_id} {...movie}/>
+    });
+    
     return (
       <div>
         <h2>Movies List</h2>
+        <ul>{movieList}</ul>
       </div>
     );
   }
